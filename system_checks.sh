aed8c0fa4b9fc70021ad0912880e73034e301cf1
Louis Denman <lmdenman02@gmail.com>
2024-05-03 15:58:36 +1000

    added important line to system_checks.sh, chmod +x in the gallo 24 bash script before the source gallo 24 line so that it will still run as an executable when downloaded and not previously chmodded.

--- system_checks.sh
+++ system_checks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ -x "$(command -v python3)" ]]
 then
+    chmod +x ./run_gallo_24_garage.sh
     source ./run_gallo_24_garage.sh
 else
     echo "It appears python is not installed on this device or you have the wrong version. Please install python version 3 or above to use this application." >&2

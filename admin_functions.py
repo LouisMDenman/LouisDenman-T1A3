2ca836d0f0a0051b10ba600c6cbaf0da141ce7ef
Louis Denman <lmdenman02@gmail.com>
2024-05-03 14:14:55 +1000

    Added far more complex comments describing individual functions and portions of code, as well as checked code to make sure it meets PEP8 coding style standards.

--- admin_functions.py
+++ admin_functions.py
@@ -1,31 +1,31 @@
-#Import python packages
+#Import python packages.
 import csv
 from random import randint
 from datetime import date
 
-#Import external packages 
+#Import external packages.
 from colored import Fore, Back, Style
 
-#establish a variable for what the date is
+#Establish a global variable for what the current date is to be used by other functions.
 today = date.today()
 
-#booking csv error message function
+#Function that is called when the user tries to make use of a function that utilises the 'bookings.csv' file when it has been deleted or isn't present. This shouldn't happen as main.py writes a new file under that name when the application is first run to avoid this issue, but this function provides a relevant error message just in case it does happen.
 def bookingcsv_error():
     print(f"{Fore.red}Booking csv file doesn't exist.{Style.reset}")
 
-#car database csv error message function
+##Function that is called when the user tries to make use of a function that utilises the 'cardatabase.csv' file when it has been deleted or isn't present. This shouldn't happen as main.py writes a new file under that name when the application is first run to avoid this issue, but this function provides a relevant error message just in case it does happen.
 def cardatabasecsv_error():
     print(f"{Fore.red}Car database csv file doesn't exist.{Style.reset}")
 
-#general issues error message function
+##Function that is called when the user creates an unexpected error that hasn't been accounted for. This shouldn't happen as code in both main.py and admin_functions.py implements thorough control structues and control flow, but in the chance that it does occur, this function provides a general error message to keep the programming from crashing.
 def general_error():
     print(f"{Fore.red}Apologies, something went wrong.{Style.reset}")
 
-#exited the game msg
+#Function that is called when a user chooses to end the game in the car guesser feature, and provides a message acknowledging that they have left the game.
 def exit_game():
     print(f"{Fore.red}\nYou have exited the game, see you next time.{Style.reset}")
 
-#bookings
+#The add_booking() function is responsible for handling adding a booking to the 'bookings.csv' file, and is called in the bookings sub loop of the main menu by selecting the add booking feature. This function first takes input and stores multiple local variables as the details required in the 'bookings.csv' file. Whilst doing so, when retrieving the desired date for a booking, the date is compared with the global variable 'today' that stores the current date, and uses a while loop to make sure that the entered date is in the future or today, not the past. The function then opens the 'bookings.csv' file in append mode, and writes the values stored in the local variables to a new line at the bottom of the file. The function then thanks the user and acknowledges that their booking has been saved 'in the system.'
 def add_booking(bookings):
     try:
         FirstName = input(f"{Fore.rgb('0%', '100%', '100%')}Please enter your first name:{Style.reset} ").capitalize()
@@ -45,6 +45,7 @@ def add_booking(bookings):
     except:
         general_error()
 
+#The remove_booking() function is responsible for handling removing a booking from the 'bookings.csv' file, and is called in the bookings sub loop of the main menu by selecting the remove booking feature. This function first takes input and stores multiple local variables as the details of the booking that the user wants to delete in the 'bookings.csv' file. Whilst doing so, when retrieving the date of the booking being deleted, the date is compared with the global variable 'today' that stores the current date, and uses a while loop to make sure that the entered date is in the future or today, not the past. Following this, the function establishes a local temporary list variable called temp_list as well as a has_booking variable with value False. The function then copies all lines of 'bookings.csv' to temp_list, unless there is a line that matches all of the inputs of the users desired appointment to delete. If this occurs, the for loop at this point simply changes has_booking to True, and moves on hence not copying that line to the temp_list. Once the for loop has finished, if the has_booking variable is True, then the temp_list is written back into the 'bookings.csv' file which does not contain the 'deleted' line, whereas if the has_booking variable was never set to True, the function warns the user that the details they provided did not match any bookings within the 'system.'
 def remove_booking(bookings):
     try:
         Fname = input(f"{Fore.rgb('0%', '100%', '100%')}\nEnter your first name to select your bookings:{Style.reset} ").capitalize()
@@ -75,6 +76,7 @@ def remove_booking(bookings):
     except:
         general_error()
 
+#The view_booking() function is responsible for handling displaying all of a users booking to them from the 'bookings.csv' file, and is called in the bookings sub loop of the main menu by selecting the view booking feature. This function first takes input in the form of the users first name and list name, so that these details may be used to check if a booking belongs to that person or not. The function then reads 'bookings.csv' and utilises a for loop with an if statement to go through each line of the file, and print the line if the first name and last name of the booking matches the user input. The variable has_booking is once again locally utlisied and is turned from False to true if there is a booking with a matching name, otherwise if this variable is False at the end of the file it prints a polite message letting the user know that there are no bookings matching their inputted name.
 def view_booking(bookings):
     try:
         Fname = input(f"{Fore.rgb('0%', '100%', '100%')}\nEnter your first name to see your booked services:{Style.reset} ").capitalize()
@@ -94,7 +96,7 @@ def view_booking(bookings):
     except:
         general_error()
 
-#information
+#The all_car_information() function is responsible for displaying all cars and their information that are stored in the 'cardatabase.csv' file to the user, and is called in the car information sub loop of the main menu by selecting the see information about all cars feature. This function is passed the global variable cardatabase, which stores the name 'cardatabase.csv' and uses it to open the file in read mode. The function then loops through the file and prints each line, producing the pieces of data in a specfic format shown in the f string to make it sound formatted and informative.
 def all_car_information(cardatabase):
     try:
         with open(cardatabase, "r") as f:
@@ -107,6 +109,7 @@ def all_car_information(cardatabase):
     except:
         general_error()
 
+#The spcefic_car_information() function is responsible for displaying a specific car and its information that is stored in the 'cardatabase.csv' file to the user, and is called in the car information sub loop of the main menu by selecting the see information about a specfic car feature. This function is passed the global variable cardatabase, which stores the name 'cardatabase.csv' and uses it to open the file in read mode. The function then loops through the file and prints a line only if the name of the brand and model of the car match the users input, wich produces the pieces of data in a specfic format shown in the f string to make it sound formatted and informative.
 def specific_car_information(cardatabase):
     try:
         CarBrand = input(f"{Fore.rgb('56%', '93%', '56%')}\nEnter the brand of car you would like to know more about:{Style.reset} ").capitalize()
@@ -126,6 +129,7 @@ def specific_car_information(cardatabase):
     except:
         general_error()
 
+#The random_car_information() function is responsible for displaying a random car and its information that is stored in the 'cardatabase.csv' file to the user, and is called in the car information sub loop of the main menu by selecting the see information about a random car feature. This function is passed the global variable cardatabase, which stores the name 'cardatabase.csv' and uses it to open the file in read mode. The function then loops through the file and copies all the lines of the file to a local variable called temp_list. A variable randnum is then established using the randint function which was imported from the random package, by taking the range between 0 and the length of the temp_list - 1 (to account for index starting at 0) and letting randint select a random number. The function then uses this random number as the index of temp_list to display, and uses the information about the car within that index to display information about the car in the informative f string.
 def random_car_information(cardatabase):
     try:
         temp_list = []
@@ -141,7 +145,7 @@ def random_car_information(cardatabase):
     except:
         general_error()
 
-#car guesser
+#The car_guess() function is responsible for displaying limited output in a game style format relating to a specfic car, and having the user try to guess the make and model of vehicle based off of the information provided. To begin, like the random_car_information() function, car_guess() copies the current 'cardatabase.csv' in read mode to a temp_list variable, and creates a randnum in the valid index range using randint from the random module. Following this, a guess variable is established as the input variable and initialised with '', as well as a count variable initialised with 0. At this point, the function imediately checks if the length of the temp_list is greater than 1, because if this is not the case it means main.py() wrote 'cardatabase.csv' at the start of the application and there is only 1 car to guess. If this else case is triggered, the program explains that the 'database' is not fully available, and we only have information on the 1 car and displays it, apologising and suggesting that it will be playable when there are more cars in the 'database.' It then also changes the value of guess to 'QUIT' so that the winners message is not played at the bottom of the function. If the temp_list length is greater than 1, the function immediately goes into a while loop that will only break when the users guess is correct. The while loop contains a long if statement that begins when count == 0, which is when the program begins, and gives the intial hint. The next elif statement will be count < 5, so that the clue is not printed every time, and just says 'try again: '. if count == 5, the clue is repeated but with slightly more detail and is followed by 'try again: ' while count < 10, which continues until count == 20. At this point, all of the clues have been given, and there is only an else statement repeating 'try again: '. Within all of these if,elif,else statements there is also a nested if statement that checks if guess == 'QUIT' which calls the exit_game() function and then utilises break to exit the while loop. At the bottom of the function as mentioned before, there is a function that check if guess != 'QUIT', and is triggered if the user did play the game and didn't quit, displaying a congratulations message that shows the correctly guessed car and the amount of guesses it took.
 def car_guess(cardatabase):
     try:
         temp_list = []
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#Import python packages
+#Import python packages.
 import os.path
 
-#Import external packages 
+#Import external packages.
 from colored import Fore, Back, Style
 
-#Imported functions written for application
+#Imported functions written for application in admin_functions.py.
 from admin_functions import *
 
-#Main function loop
+#Main menu function used for printing output and recieving user input that detirmines what feature to use.
 def main():
     print(f"{Fore.rgb('53%', '12%', '47%')}\nHey, Welcome to Gallo 24 Car Garage! \n")
     print("Enter 1 to add, remove or view a booking.")
@@ -18,7 +18,7 @@ def main():
     user_input = input(f":{Style.reset} ")
     return user_input
 
-#Booking function loop
+#Sub menu function for the booking feature, responsible for printing output and recieving user input to detirmine if the user wants to add, remove, view booking/(s), or return to the main menu.
 def booking_feature():
         print(f"{Fore.rgb('0%', '100%', '100%')}\nEnter 1 to add a booking.")
         print("Enter 2 to remove a booking.")
@@ -28,7 +28,7 @@ def booking_feature():
         user_input = input(f":{Style.reset} ")
         return user_input
 
-#Car information loop
+#Sub menu function for the car information feature, responsible for printing output and recieving user input to detirmine if the user wants to see information on all available cars, a specific car, a random car, or return to the main menu.
 def carinfo_feature():
         print(f"{Fore.rgb('56%', '93%', '56%')}\nEnter 1 to see information on all cars in our database.")
         print("Enter 2 to see information on a specfic car.")
@@ -38,7 +38,7 @@ def carinfo_feature():
         user_input = input(f":{Style.reset} ")
         return user_input
 
-#Car guesser loop
+#Sub menu function for the car guesser feature, responsible for printing output and recieving user input to detirmine if the user wants to play the guessing game, or return to the main menu.
 def carguess_feature():
         print(f"{Fore.rgb('100%', '84%', '0%')}\nEnter 1 to guess a random car selected from out database. You will get increasing hints with more guesses, and if you would like to exit or cannot guess the vehicle once in the game, type quit.")
         print("Enter 2 to return to main menu.\n")
@@ -46,35 +46,35 @@ def carguess_feature():
         user_input = input(f":{Style.reset} ")
         return user_input
 
-#Wrong input
+#A simple function that is called when misinput is entered at both the main menu and sub menu levels, and informs the users that they must enter an option displayed above in the relevant menu.
 def wrong_input():
     print(f"{Fore.red}\nPlease enter one of the options shown above.{Style.reset}")
 
-#assign variables required csv names
+#Assign the names of the required csv files for the application to global variables, so that they can be used to check if those file names exist in the directory and write them if they are not.
 cardatabase = 'cardatabase.csv'
 bookings = 'bookings.csv'
 
-#write bookings csv function
+#Function that creates a new csv file called 'bookings.csv' which is a required file for the application to run.
 def write_bookings_csv():
     file = open(bookings, 'w')
     file.write("LastName,FirstName,Date,CarBrand,CarModel,Service\n")
     file.close()
 
-#write car database csv function
+#Function that creates a new csv file called 'cardatabase.csv' which is a required file for the application to run.
 def write_cardatabase_csv():
     file = open(cardatabase, 'w')
     file.write("CarBrand,CarModel,BodyType,0To60,Cylinders,Aspirated,Horsepower,TopSpeed\nSubaru,BRZ,coupe,7.5,4,naturally aspirated,228,220")
     file.close()
 
-#Check if booking csv doesn't exist and writes a file if that is the case
+#Checks if the 'bookings.csv' file exists within the directory, which is a required file for the application to run all the booking features, and calls on the write_bookings_csv() function if it cannot find the file.
 if (not os.path.isfile(bookings)):
     write_bookings_csv()
 
-#Check if car database csv doesn't exist and writes a file if that is the case
+#Checks if the 'cardatabase.csv' file exists within the directory, which is a required file for the application to run all the car information and guesser features, and calls on the write_cardatabase_csv() function if it cannot find the file.
 if (not os.path.isfile(cardatabase)):
     write_cardatabase_csv()
 
-#initialise the service variable and commence the main menu loop
+#Initialises the service variable, which is then given the main() functions input which is entered by the user to control what they would like to do. Once the user selects a service, the same process occurs in a nested loop for each feature, with the feature variable being established and used to detirmine what the user would like to do in any given sub loop.
 service = ''
 while service != '4':
     service = main()
@@ -124,5 +124,5 @@ while service != '4':
     else:
         wrong_input()
 
-#Print a goodbye message
+#Print a polite goodbye message when the user chooses to terminal the application.
 print(f"{Fore.red}\nThanks for visiting the Gallo 24 Car Garage, until next time!{Style.reset}")
